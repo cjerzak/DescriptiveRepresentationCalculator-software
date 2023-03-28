d3fa3e393ddccd28d2d9adc763597c8718baa267
cjerzak <connor.jerzak@gmail.com>
2023-03-28 08:57:45 -0500

    up

--- documentPackage.R
+++ documentPackage.R
@@ -1,9 +1,9 @@
 setwd("~/Documents/ExpectedRepresentationCalculator-software")
 
-package_path <- "~/Documents/ExpectedRepresentationCalculator-software/ExpectedRepresentationCalculator"
+package_path <- "~/Documents/ExpectedRepresentationCalculator-software/DescriptiveRepresentationCalculator"
 
 devtools::document(package_path)
-try(file.remove("./ExpectedRepresentationCalculator.pdf"),T)
+try(file.remove("./DescriptiveRepresentationCalculator.pdf"),T)
 system(paste(shQuote(file.path(R.home("bin"), "R")),
              "CMD", "Rd2pdf", shQuote(package_path)))
 
