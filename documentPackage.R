558ae72f582e8e08a391ecd0ade46d766fd8f9fe
cjerzak <connor.jerzak@gmail.com>
2023-10-13 10:23:26 -0500

    tar

--- documentPackage.R
+++ documentPackage.R
@@ -1,6 +1,7 @@
 setwd("~/Documents/DescriptiveRepresentationCalculator-software")
 package_path <- "~/Documents/DescriptiveRepresentationCalculator-software/DescriptiveRepresentationCalculator"
 
+# build documentation
 devtools::document(package_path)
 try(file.remove("./DescriptiveRepresentationCalculator.pdf"),T)
 system(paste(shQuote(file.path(R.home("bin"), "R")),
@@ -9,3 +10,6 @@ system(paste(shQuote(file.path(R.home("bin"), "R")),
 # Check package to ensure it meets CRAN standards.
 devtools::check( package_path )
 
+# build tar
+system( paste(shQuote(file.path(R.home("bin"), "R")),
+              "R CMD build", shQuote(package_path)) )
