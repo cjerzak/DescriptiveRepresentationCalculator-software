07b44a214bff800ae77fcd10fb27e63385885c2e
cjerzak <connor.jerzak@gmail.com>
2025-06-11 13:52:16 -0400

    up

--- documentPackage.R
+++ documentPackage.R
@@ -1,34 +1,31 @@
 {
-# setup environment
-rm(  list = ls()  )
-setwd("~/Documents/DescriptiveRepresentationCalculator-software")
-PackageName <- "DescriptiveRepresentationCalculator"
-package_path <- sprintf("~/Documents/%s-software/%s",PackageName,PackageName)
-versionNumber <- "1.1.0"
-
-
-# build documentation
-library(devtools); library(utils)
-devtools::document(package_path)
-try(file.remove("./DescriptiveRepresentationCalculator.pdf"),T)
-devtools::document(package_path)
-#devtools::build_manual(package_path)
-system(paste(shQuote(file.path(R.home("bin"), "R")),
-             "CMD", "Rd2pdf", shQuote(package_path)))
-
-setwd("~/Documents/")
-
-# build tar
-system( paste(shQuote(file.path(R.home("bin"), "R")),
-              "R CMD build --resave-data", shQuote(package_path)) )
-
-# check package to ensure it meets CRAN standards.
-devtools::check( package_path, cran = T )
-
-# check as cran
-system( paste(shQuote(file.path(R.home("bin"), "R")),
-              "R CMD check --as-cran",
-              shQuote(
-                paste(PackageName, "_", versionNumber, ".tar.gz", sep = "")
-              )) )
+  # setup environment
+  rm(  list = ls()  )
+  setwd("~/Documents/DescriptiveRepresentationCalculator-software")
+  PackageName <- "DescriptiveRepresentationCalculator"
+  package_path <- sprintf("~/Documents/%s-software/%s",PackageName,PackageName)
+  versionNumber <- "1.1.0"
+  
+  # build documentation
+  library(devtools); library(utils)
+  try(file.remove("./DescriptiveRepresentationCalculator.pdf"),T)
+  devtools::document(package_path)
+  system(paste(shQuote(file.path(R.home("bin"), "R")),
+               "CMD", "Rd2pdf", shQuote(package_path)))
+  
+  setwd("~/Documents/")
+  
+  # build tar
+  system( paste(shQuote(file.path(R.home("bin"), "R")),
+                "R CMD build --resave-data", shQuote(package_path)) )
+  
+  # check package to ensure it meets CRAN standards.
+  devtools::check( package_path, cran = T )
+  
+  # check as cran
+  system( paste(shQuote(file.path(R.home("bin"), "R")),
+                "R CMD check --as-cran",
+                shQuote(
+                  paste(PackageName, "_", versionNumber, ".tar.gz", sep = "")
+                )) )
 }
